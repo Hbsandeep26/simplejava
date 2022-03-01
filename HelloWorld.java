1381ed6722517bdda51a7d62a3d8f86f5c48e964
EC2 Default User <ec2-user@ip-172-31-11-122.ap-south-1.compute.internal>
2022-03-01 14:08:11 +0000

    some msg

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-     System.out.println("Helo");
+     System.out.println("elo");
     }  
 }  
