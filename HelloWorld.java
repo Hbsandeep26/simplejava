d2c5568493c9f7966e444840b19f7056ef57c1cd
EC2 Default User <ec2-user@ip-172-31-11-122.ap-south-1.compute.internal>
2022-03-01 14:04:50 +0000

    updated the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-     System.out.println("Hello Ja");
+     System.out.println("Hello");
     }  
 }  
